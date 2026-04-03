eb4e8398867dd63d0aa2482f5c951d0d551c0435
justinr <justin@apsis.sh>
2026-04-02 21:04:37 -0700

    keyball39 can now scroll horizontally

--- keyboards/keyball/keyball39/keymaps/custom/config.h
+++ keyboards/keyball/keyball39/keymaps/custom/config.h
@@ -38,6 +38,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 // #define AUTO_MOUSE_DEFAULT_LAYER 2
 
+#define KEYBALL_SCROLLSNAP_ENABLE 0 // 0 allows horizontal scrolling
+
 #define DYNAMIC_KEYMAP_LAYER_COUNT 8
 
 // for homerow mods
