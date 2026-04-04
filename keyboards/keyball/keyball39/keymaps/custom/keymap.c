44615da750243d3504d506904cac9ecc8270c088
justinr <justin@apsis.sh>
2026-04-04 00:44:06 -0700

    Moved modifiers on keyball39 layer 6 to correct row.

--- keyboards/keyball/keyball39/keymaps/custom/keymap.c
+++ keyboards/keyball/keyball39/keymaps/custom/keymap.c
@@ -89,8 +89,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [6] = LAYOUT_universal(
     _______ , _______ , _______ , _______ , _______ ,                            KC_PSCR , KC_F7 , KC_F8 , KC_F9 , KC_F12 ,
-    _______ , _______ , _______ , _______ , _______ ,                            KC_SCRL , KC_F4 , KC_F5 , KC_F6 , KC_F11 ,
-    KC_LALT , KC_LCTL , KC_LGUI , KC_LSFT , _______ ,                        KC_PAUS , KC_F1 , KC_F2 , KC_F3 , KC_F10 ,
+    KC_LALT , KC_LCTL , KC_LGUI , KC_LSFT , _______ ,                            KC_SCRL , KC_F4 , KC_F5 , KC_F6 , KC_F11 ,
+    _______ , _______ , _______ , _______ , _______ ,                        KC_PAUS , KC_F1 , KC_F2 , KC_F3 , KC_F10 ,
     _______ , _______ , _______ , _______ , _______ , KC_TRNS ,      _______ ,   _______ , _______ , _______ , _______ , _______
   ),
 
