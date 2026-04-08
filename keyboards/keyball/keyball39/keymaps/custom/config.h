7c2ad889d59dd86ebc085661e9d3becea07619c1
justinr <justin@apsis.sh>
2026-04-07 23:07:28 -0700

    Changed flow tap term delay to 75 on keyball39.

--- keyboards/keyball/keyball39/keymaps/custom/config.h
+++ keyboards/keyball/keyball39/keymaps/custom/config.h
@@ -52,6 +52,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // see: https://docs.qmk.fm/tap_hold
 #define TAPPING_TERM 150
 // #define QUICK_TAP_TERM 120  // defaults to TAPPING_TERM, prevents double-tap-to-repeat from blocking mod activation
-#define FLOW_TAP_TERM 80
+#define FLOW_TAP_TERM 75
 #define HOLD_ON_OTHER_KEY_PRESS
 #define PERMISSIVE_HOLD
