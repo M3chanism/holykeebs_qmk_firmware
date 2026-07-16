529dcbb020f146fc07b56f936e5b158ebfae7c06
justinr <justin@apsis.sh>
2026-07-15 23:29:26 -0700

    convert keyball39 to colemak

--- keyboards/keyball/keyball39/keymaps/custom/keymap.c
+++ keyboards/keyball/keyball39/keymaps/custom/keymap.c
@@ -21,30 +21,37 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "quantum.h"
 
 // Home row mod definitions
-#define HM_A  MT(MOD_LALT, KC_A)
+#define HM_A  MT(MOD_LGUI, KC_A)
+#define HM_R  MT(MOD_LALT, KC_R)
 #define HM_S  MT(MOD_LCTL, KC_S)
-#define HM_D  MT(MOD_LGUI, KC_D)
-#define HM_F  MT(MOD_LSFT, KC_F)
-#define HM_J  MT(MOD_RSFT, KC_J)
-#define HM_K  MT(MOD_RGUI, KC_K)
-#define HM_L  MT(MOD_RCTL, KC_L)
-#define HM_QT MT(MOD_RALT, KC_QUOT)
+#define HM_T  MT(MOD_LSFT, KC_T)
+#define HM_N  MT(MOD_RSFT, KC_N)
+#define HM_E  MT(MOD_RCTL, KC_E)
+#define HM_I  MT(MOD_RALT, KC_I)
+#define HM_O  MT(MOD_RGUI, KC_O)
+
+// copy, cut, paste undo, redo
+#define M_REDO LCTL(KC_Y)
+#define M_PASTE LCTL(KC_V)
+#define M_COPY LCTL(KC_C)
+#define M_CUT  LCTL(KC_X)
+#define M_UNDO LCTL(KC_Z)
 
 // Set tap hold delay for each modifier individually
 uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t* record, uint16_t prev_keycode) {
     if (is_flow_tap_key(keycode) && is_flow_tap_key(prev_keycode)) {
       switch (keycode) {
-          case HM_F:
-          case HM_J:
+          case HM_T:
+          case HM_N:
               return 0;
-          // case HM_D:
-          // case HM_K:
-          //     return FLOW_TAP_TERM - 25; // subtract 25ms from global value
           // case HM_S:
-          // case HM_L:
+          // case HM_E:
+          //     return FLOW_TAP_TERM - 25; // subtract 25ms from global value
+          // case HM_R:
+          // case HM_I:
           //     return 150;
           // case HM_A:
-          // case HM_QT:
+          // case HM_O:
           //     return 150;
           default:
               return FLOW_TAP_TERM;  // use the global macro directly
@@ -79,52 +86,52 @@ void keyball_on_apply_motion_to_mouse_scroll(report_mouse_t *r, report_mouse_t *
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default (VIA)
   [0] = LAYOUT_universal(
-    KC_Q, KC_W, KC_E, KC_R, KC_T,              KC_Y, KC_U, KC_I, KC_O, KC_P,
-    HM_A, HM_S, HM_D, HM_F, KC_G,              KC_H, HM_J, HM_K, HM_L, HM_QT,
-    KC_Z, KC_X, LT(2,KC_C), KC_V, KC_B,        KC_N, KC_M, LT(2,KC_COMM), KC_DOT, KC_SLSH,
+    KC_Q, KC_W, KC_F, KC_P, KC_B,              KC_J, KC_L, KC_U, KC_Y, KC_QUOT,
+    HM_A, HM_R, HM_S, HM_T, KC_G,              KC_M, HM_N, HM_E, HM_I, HM_O,
+    KC_Z, KC_X, LT(2,KC_C), KC_D, KC_V,        KC_K, KC_H, LT(2,KC_COMM), KC_DOT, KC_SLSH,
     KC_NO,KC_NO,KC_NO , LT(3,KC_ESC),LT(1,KC_SPC),LT(6,KC_TAB),      LT(5,KC_ENT),LT(4,KC_BSPC),  KC_NO,KC_NO,KC_NO,     KC_NO
   ),
 
   [1] = LAYOUT_universal(
-    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO  ,                           KC_NO, KC_CAPS, KC_INS, KC_NO, KC_NO,
-    KC_LALT  , KC_LCTL , KC_LGUI , KC_LSFT , KC_NO ,                           KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_DEL,
-    KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,                                                 KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO,
+    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO  ,                           M_REDO, M_PASTE, M_COPY, M_CUT, M_UNDO,
+    KC_LGUI  , KC_LALT , KC_LCTL , KC_LSFT , KC_NO ,                           KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_DEL,
+    KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,                                                 KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_INS,
     KC_NO,KC_NO,KC_NO         , _______  , KC_TRNS , _______  ,       KC_ENT, KC_BSPC,        KC_NO,KC_NO,KC_NO,     KC_NO
   ),
 
   [2] = LAYOUT_universal(
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                             KC_NO, SCRL_DVI, SCRL_DVD, KC_NO, KC_NO,
-    KC_LALT, KC_LCTL, KC_LGUI , KC_LSFT , KC_NO ,                              KC_NO, KC_RSFT, KC_RGUI, KC_RCTL, KC_RALT,
+    QK_BOOT, EE_CLR, KC_NO, KC_NO, KC_NO,                                             M_REDO, M_PASTE, M_COPY, M_CUT, M_UNDO,
+    KC_NO, KC_NO, KC_NO , KC_NO , KC_NO ,                                          KC_NO, SCRL_DVD, SCRL_DVI, KC_NO, KC_NO,
     KC_NO, KC_NO, KC_TRNS, SCRL_MO, KC_NO,                                         KC_NO, SCRL_MO, KC_TRNS, KC_NO, KC_NO,
     KC_NO,KC_NO,KC_NO,           KC_MS_BTN2, KC_MS_BTN1, KC_MS_BTN3,       KC_MS_BTN3, KC_MS_BTN1,    KC_NO,KC_NO,KC_NO,     KC_NO
   ),
 
   [3] = LAYOUT_universal(
     _______ , _______ , _______ , _______ , _______ ,                            RGB_RMOD , RGB_VAD , RGB_VAI , RGB_MOD , _______ ,
-    _______ , KC_MPRV , KC_VOLU , KC_VOLD , KC_MNXT ,                            KC_MPRV , KC_VOLD , KC_VOLU , KC_MNXT , _______ ,
+    KC_MUTE , KC_MPRV , KC_VOLU , KC_VOLD , KC_MNXT ,                            KC_MPRV , KC_VOLD , KC_VOLU , KC_MNXT , KC_MUTE ,
     _______ , _______ , _______ , _______ , _______ ,                            _______ , _______ , _______ , _______ , _______ ,
     _______ , _______ , _______ ,      KC_TRNS , KC_MPLY , KC_MSTP ,      KC_MSTP , KC_MPLY ,       _______ , _______ , _______ , QK_BOOT
   ),
 
   [4] = LAYOUT_universal(
     KC_LBRC , KC_7 , KC_8 , KC_9 , KC_RBRC ,                            _______ , _______ , _______ , _______ , _______ ,
-    KC_SCLN , KC_4 , KC_5 , KC_6 , KC_PEQL ,                            _______ , KC_RSFT , KC_RGUI , KC_RCTL , KC_RALT ,
+    KC_SCLN , KC_4 , KC_5 , KC_6 , KC_PEQL ,                            _______ , KC_RSFT , KC_RCTL , KC_RALT , KC_RGUI ,
     KC_GRV , KC_1 , KC_2 , KC_3 , KC_BSLS ,                            _______ , _______ , _______ , _______ , _______ ,
     _______ , _______ , _______ , KC_PDOT , KC_P0 , KC_PMNS ,      _______ ,   KC_TRNS , _______ , _______ , _______ , _______
   ),
 
   [5] = LAYOUT_universal(
     S(KC_LBRC) , S(KC_7) , S(KC_8) , S(KC_9) , S(KC_RBRC) ,                      _______ , _______ , _______ , _______ , _______ ,
-    S(KC_SCLN) , S(KC_4) , S(KC_5) , S(KC_6) , KC_PPLS ,                            _______ , KC_RSFT , KC_RGUI , KC_RCTL , KC_RALT ,
+    S(KC_SCLN) , S(KC_4) , S(KC_5) , S(KC_6) , KC_PPLS ,                            _______ , KC_RSFT , KC_RCTL , KC_RALT , KC_RGUI ,
     S(KC_GRV) , S(KC_1) , S(KC_2) , S(KC_3) , S(KC_BSLS) ,                            _______ , _______ , _______ , _______ , _______ ,
     _______ , _______ , _______ , S(KC_9) , S(KC_0) , S(KC_MINS) ,      KC_TRNS ,   _______ , _______ , _______ , _______ , _______
   ),
 
   [6] = LAYOUT_universal(
     _______ , _______ , _______ , _______ , _______ ,                            KC_PSCR , KC_F7 , KC_F8 , KC_F9 , KC_F12 ,
-    KC_LALT , KC_LCTL , KC_LGUI , KC_LSFT , _______ ,                            KC_SCRL , KC_F4 , KC_F5 , KC_F6 , KC_F11 ,
-    _______ , _______ , _______ , _______ , _______ ,                        KC_PAUS , KC_F1 , KC_F2 , KC_F3 , KC_F10 ,
-    _______ , _______ , _______ , _______ , _______ , KC_TRNS ,      _______ ,   _______ , _______ , _______ , _______ , _______
+    KC_LGUI , KC_LALT , KC_LCTL , KC_LSFT , _______ ,                            KC_SCRL , KC_F4 , KC_F5 , KC_F6 , KC_F11 ,
+    _______ , _______ , _______ , _______ , _______ ,                            KC_PAUS , KC_F1 , KC_F2 , KC_F3 , KC_F10 ,
+    _______ , _______ , _______ , _______ , _______ , KC_TRNS ,      _______ ,   KC_APP , _______ , _______ , _______ , _______
   ),
 
   [7] = LAYOUT_universal(
